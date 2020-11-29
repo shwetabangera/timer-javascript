c3d54539466fce71cd1628d608c958e408bcc54e
shwetabangera <shweta.bangera@raweng.com>
2020-11-29 23:20:22 +0530

    used little different logic for timer and changed style

--- src/timer.js
+++ src/timer.js
@@ -1,34 +1,39 @@
-window.onload = () => {
-  let hour = 0;
-  let minute = 0;
-  let seconds = 0;
-  let totalSeconds = 0;
-
-  let intervalId = null;
-
-  function startTimer() {
-    ++totalSeconds;
-    hour = Math.floor(totalSeconds / 3600);
-    minute = Math.floor((totalSeconds - hour * 3600) / 60);
-    seconds = totalSeconds - (hour * 3600 + minute * 60);
-
-    document.getElementById("hour").innerHTML = hour;
-    document.getElementById("minute").innerHTML = minute;
-    document.getElementById("seconds").innerHTML = seconds;
+let h1 = document.getElementsByTagName("h1")[0],
+  start = document.getElementById("start"),
+  stop = document.getElementById("pause"),
+  clear = document.getElementById("reset"),
+  seconds = 0,
+  minutes = 0,
+  hours = 0,
+  t;
+function add() {
+  seconds++;
+  if (seconds >= 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes >= 60) {
+      minutes = 0;
+      hours++;
+    }
   }
-
-  document.getElementById("start").addEventListener("click", () => {
-    intervalId = setInterval(startTimer, 1000);
-  });
-
-  document.getElementById("pause").addEventListener("click", () => {
-    if (intervalId) clearInterval(intervalId);
-  });
-
-  document.getElementById("reset").addEventListener("click", () => {
-    totalSeconds = 0;
-    document.getElementById("hour").innerHTML = "0 ";
-    document.getElementById("minute").innerHTML = "0 ";
-    document.getElementById("seconds").innerHTML = "0 ";
-  });
+  h1.textContent =
+    (hours ? (hours > 9 ? hours : "0" + hours) : "00") +
+    ":" +
+    (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") +
+    ":" +
+    (seconds > 9 ? seconds : "0" + seconds);
+  timer();
+}
+function timer() {
+  t = setTimeout(add, 1000);
+}
+start.onclick = timer;
+stop.onclick = function () {
+  clearTimeout(t);
+};
+clear.onclick = function () {
+  h1.textContent = "00:00:00";
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
 };
